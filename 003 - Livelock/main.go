10a392f038e916d37ef7a64a3dbcf27a5d7109da
Dmitry Slobodchikov <zoosman@mail.ru>
2021-04-16 20:29:25 +0500

    Fix minors

--- 003 - Livelock/main.go
+++ 003 - Livelock/main.go
@@ -10,11 +10,6 @@ import (
 	"time"
 )
 
-type value struct {
-	mu    sync.Mutex
-	value int
-}
-
 func main() {
 	fmt.Println("003 - Livelock")
 
@@ -68,7 +63,7 @@ func main() {
 				return
 			}
 		}
-		fmt.Fprintf(&out, "\n%v tosses her hands up in exasperatino!", name)
+		fmt.Fprintf(&out, "\n%v tosses her hands up in exasperation!", name)
 	}
 
 	var peopleInHallway sync.WaitGroup
@@ -76,26 +71,4 @@ func main() {
 	go walk(&peopleInHallway, "Alice")
 	go walk(&peopleInHallway, "Barnara")
 	peopleInHallway.Wait()
-
-	// var wg sync.WaitGroup
-
-	// printSum := func(v1, v2 *value) {
-	// 	defer wg.Done()
-	// 	v1.mu.Lock()
-	// 	defer v1.mu.Unlock()
-	// 	time.Sleep(2 * time.Second)
-	// 	v2.mu.Lock()
-	// 	defer v2.mu.Unlock()
-
-	// 	fmt.Printf("the sum is %v. \n", v1.value+v2.value)
-	// }
-
-	// var a, b value
-
-	// wg.Add(2)
-
-	// go printSum(&a, &b)
-	// go printSum(&b, &a)
-
-	// wg.Wait()
 }
